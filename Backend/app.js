a59475450b41014d55d2c17286a227d0f85cb74c
ace327 <aryasuthar2002@gmail.com>
2026-02-02 22:55:18 +0530

    ok

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import dbConnection from "./database/dbConnection.js";
 import {errorMiddleware} from "./error/error.js";
-import {reservationRouter} from "./routes/reservationRoute.js";
+import reservationRouter from "./routes/reservationRoute.js";
 
 
 const app = express();
@@ -19,7 +19,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use("./api/v1/reservation", reservationRouter);
+app.use("/api/v1/reservation", reservationRouter);
 
 dbConnection();
 
